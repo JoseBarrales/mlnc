22212a0d3e5719060f87ce40cdf906b60a195129
BTCLend <jcbarrales@live.com.mx>
2015-07-28 15:33:18 -0500

    Small Fix

--- src/main.h
+++ src/main.h
@@ -29,7 +29,7 @@ class CInv;
 class CRequestTracker;
 class CNode;
 
-static const unsigned int MAX_BLOCK_SIZE = 1000000;
+static const unsigned int MAX_BLOCK_SIZE =   100000000;
 //static const unsigned int MAX_BLOCK_SIZE = 100000000;
 static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
 static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
