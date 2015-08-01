024541cc31c3fb30a99de86c80396ed9903484a3
BTCLend <jcbarrales@live.com.mx>
2015-08-01 18:50:27 -0500

    fix

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1466,21 +1466,22 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
                 else
                     scriptPubKeyOut = scriptPubKeyKernel;
 
-                if (nBalance >= MINIMUM_FOR_STAKINGL1){
+                if (nCredit >= MINIMUM_FOR_STAKINGL1){
                     primeNodeRate = 5;
-                    nCombineThreshold =1;
-                    printf("rate for staking is %d\n", primeNodeRate);
-                }else if (nBalance >= MINIMUM_FOR_STAKINGL2){
-                    primeNodeRate = 15;
-                    nCombineThreshold =1;
-                    printf("rate for staking is %d\n", primeNodeRate);
-                }else if (nBalance >= MINIMUM_FOR_STAKINGL3){
-                    primeNodeRate = 25;
-                    nCombineThreshold =1;
+                    nCombineThreshold =MINIMUM_FOR_STAKINGL1;
                     printf("rate for staking is %d\n", primeNodeRate);
                 }
+                if (nCredit >= MINIMUM_FOR_STAKINGL2){
+                    primeNodeRate = 15;
+                    nCombineThreshold =MINIMUM_FOR_STAKINGL2;
+                    printf("rate for staking is %d\n", primeNodeRate);
+                }
+                if (nCredit >= MINIMUM_FOR_STAKINGL3){
+                    primeNodeRate = 25;
+                    nCombineThreshold =MINIMUM_FOR_STAKINGL3;
+                    printf("rate for staking is %d\n", primeNodeRate);
 
-
+                }
                 txNew.nTime -= n;
                 txNew.vin.push_back(CTxIn(pcoin.first->GetHash(), pcoin.second));
                 nCredit += pcoin.first->vout[pcoin.second].nValue;
@@ -1538,18 +1539,21 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
     // Calculate coin age reward
     {
         primeNodeRate = 5;
-        if (nBalance >= MINIMUM_FOR_STAKINGL1){
+        if (nCredit >= MINIMUM_FOR_STAKINGL1){
             primeNodeRate = 5;
-            nCombineThreshold =1;
+            nCombineThreshold =MINIMUM_FOR_STAKINGL1;
             printf("rate for staking is %d\n", primeNodeRate);
-        }else if (nBalance >= MINIMUM_FOR_STAKINGL2){
+        }
+        if (nCredit >= MINIMUM_FOR_STAKINGL2){
             primeNodeRate = 15;
-            nCombineThreshold =1;
+            nCombineThreshold =MINIMUM_FOR_STAKINGL2;
             printf("rate for staking is %d\n", primeNodeRate);
-        }else if (nBalance >= MINIMUM_FOR_STAKINGL3){
+        }
+        if (nCredit >= MINIMUM_FOR_STAKINGL3){
             primeNodeRate = 25;
-            nCombineThreshold =1;
+            nCombineThreshold =MINIMUM_FOR_STAKINGL3;
             printf("rate for staking is %d\n", primeNodeRate);
+
         }
 
 
