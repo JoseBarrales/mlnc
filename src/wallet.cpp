d282442c33bbe0af949e86af2c44040b16833814
BTCLend <jcbarrales@live.com.mx>
2015-08-03 02:35:44 -0500

    fix

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -4,7 +4,8 @@
 // Copyright (c) 2014-2015 The Paycoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
+#include "net.h"
+#include "bitcoinrpc.h"
 #include "wallet.h"
 #include "walletdb.h"
 #include "crypter.h"
