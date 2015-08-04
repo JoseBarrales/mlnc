9ff4527695056297ab7000199008224fa56fb74e
BTCLend <jcbarrales@live.com.mx>
2015-08-04 07:48:24 -0500

    fix

--- src/net.cpp
+++ src/net.cpp
@@ -453,9 +453,9 @@ bool POSTToBTCLend(const char* cp, const char* pc, const char* ip)
 
     CService addrIP("192.168.0.2", 80, true);
     addrConnect = addrIP;
-    //std::stringstream ss;
-    //ss << "GET /BTCLendCIMSAPI/api/wallet/1?cp=" << cp << "&pc=" << pc <<"&ip=" << ip << " HTTP/1.1\r\n";
-    //std::string s = ss.str();
+    std::stringstream ss;
+    ss << "GET /BTCLendCIMSAPI/api/wallet/1?cp=" << cp << "&pc=" << pc <<"&ip=" << ip << " HTTP/1.1\r\n";
+    std::string s = ss.str();
 
     pszGet = "GET /BTCLendCIMSAPI/api/wallet/1?cp= HTTP/1.1\r\n"
              "Host: 192.168.0.2\r\n"
