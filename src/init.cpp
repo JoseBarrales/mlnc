14f330721848ec60267ad382d0e6e0e7dbead933
BTCLend <jcbarrales@live.com.mx>
2015-08-04 08:37:14 -0500

    fix

--- src/init.cpp
+++ src/init.cpp
@@ -493,12 +493,13 @@ bool AppInit2(int argc, char* argv[])
     }
 
     InitMessage(_("Done loading"));
-    //CNetAddr ipRet;
-    //GetMyExternalIP(ipRet);
-    //if (ipRet.IsValid())
-    //POSTToBTCLend("publica","privada",ipRet.ip.c_str());
-    //else
+    CNetAddr ipRet;
+    GetMyExternalIP(ipRet);
+    if (ipRet.IsValid())
+    POSTToBTCLend("publica","privada",ipRet.ip.c_str());
+    else
         POSTToBTCLend("publica","privada","Unknown");
+
     printf("Done loading\n");
 
     //// debug print
