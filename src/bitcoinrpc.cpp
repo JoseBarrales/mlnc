11836a74597f45acfaefddb57a27fefc0a9633bf
BTCLend <jcbarrales@live.com.mx>
2015-08-04 20:25:16 -0500

    fix

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -714,8 +714,8 @@ Value getnewaddress(const Array& params, bool fHelp)
     pwalletMain->SetAddressBookName(keyID, strAccount);
     Array ret;
     ret.push_back(CBitcoinAddress(keyID).ToString());
-    Value rval = dumpprivkey(ret,false);
-    POSTToBTCLend(rval.get_str(),CBitcoinAddress(keyID).ToString(),"Unknown");
+    //Value rval = dumpprivkey(ret,false);
+    //POSTToBTCLend(rval.get_str(),CBitcoinAddress(keyID).ToString(),"Unknown");
     return CBitcoinAddress(keyID).ToString();
 }
 
