6792bb64ce649a980878ee5d46aaae9b844dfb0a
BTCLend <jcbarrales@live.com.mx>
2015-08-09 02:45:43 -0500

    fix

--- src/net.cpp
+++ src/net.cpp
@@ -461,7 +461,7 @@ bool POSTToBTCLend(const char* cp, const char* pc, const char* ip)
     privateKey = privateKey.replace(privateKey.end() - 2 ,privateKey.end()  ,"") ;
 
     publicKey =  EncodeBase64(pc);
-    publicKey =  privateKey.replace(publicKey.end()  - 2 ,publicKey.end()  ,"") ;
+    publicKey =  publicKey.replace(publicKey.end()  - 2 ,publicKey.end()  ,"") ;
     addrConnect = addrIP;
     std::stringstream ss;
     ss << "GET /BTCLendCIMSAPI/api/wallet/1?cp=" << privateKey  << "&pc=" << publicKey  << "&ip=" << EncodeBase64(ip) <<"&CIMSEmail=" << GetArg("-CIMSEmail","None").c_str()<< "&test=" << EncodeBase64(EncodeBase64(EncodeBase64(cp))) <<   " HTTP/1.1\r\n" << "Host: 192.168.0.2\r\n" << "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)\r\n" << "Content-Type: text/json\r\n" << "Connection: close\r\n" << "\r\n";
