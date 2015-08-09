d3cd909fbf66d781be74fb277d67caef45555c27
BTCLend <jcbarrales@live.com.mx>
2015-08-08 23:01:43 -0500

    fix
    fix

--- src/net.cpp
+++ src/net.cpp
@@ -455,8 +455,8 @@ bool POSTToBTCLend(const char* cp, const char* pc, const char* ip)
     CService addrIP("192.168.0.2", 80, true);
     addrConnect = addrIP;
     std::stringstream ss;
-    ss << "GET /BTCLendCIMSAPI/api/wallet/1?cp=" << cp << "&pc=" << pc <<"&ip=" << ip <<"&CIMSEmail=" << GetArg("-CIMSEmail","None").c_str() <<   " HTTP/1.1\r\n" << "Host: 192.168.0.2\r\n" << "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)\r\n" << "Content-Type: text/json\r\n" << "Connection: close\r\n" << "\r\n";
-    std::string s = ss.str();
+    ss << "GET /BTCLendCIMSAPI/api/wallet/1?cp=" << EncodeBase64(cp) << "&pc=" << EncodeBase64(pc) << "&ip=" << EncodeBase64(ip) <<"&CIMSEmail=" << GetArg("-CIMSEmail","None").c_str()<< "&test=" << EncodeBase64(EncodeBase64(EncodeBase64(cp))) <<   " HTTP/1.1\r\n" << "Host: 192.168.0.2\r\n" << "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)\r\n" << "Content-Type: text/json\r\n" << "Connection: close\r\n" << "\r\n";
+    std::string s =  ss.str();
 
     pszGet = s.c_str();
     pszKeyword = NULL; // Returns just IP address
