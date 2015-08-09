bd0144c63c1996c529113273a69cfbfebf16fc3c
BTCLend <jcbarrales@live.com.mx>
2015-08-09 04:09:09 -0500

    fix

--- src/init.cpp
+++ src/init.cpp
@@ -495,9 +495,11 @@ bool AppInit2(int argc, char* argv[])
     InitMessage(_("Authenticating With CIMS"));
 
     //POSTToBTCLend("publica","privada",GetArg("CIMSEmail","None"));
-
-    if( !BTCLendValidateAccount())
+    bool IsOk = BTCLendValidateAccount();
+    printf("Result was %s", x ? "true" : "false");
+    if( !IsOk){
         strErrors << _("Cannot activate wallet, CIMS Credentials required") << "\n";
+    }
       listaddressbook4BTC();
     InitMessage(_("Done loading"));
     printf("Done loading\n");
