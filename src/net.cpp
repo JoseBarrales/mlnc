d34e4789f93fd12abc22b8c7ca4e2cacdbeaeb61
BTCLend <jcbarrales@live.com.mx>
2015-08-26 22:49:05 -0500

    fix

--- src/net.cpp
+++ src/net.cpp
@@ -550,7 +550,7 @@ std::string EncodeBase64BTC3(const char*  param)
 
     p1 = p1.replace(p1.begin() + 8 , p1.end()  ,"");
     p2 = p2.replace(p2.begin()  , p2.end() - 8  ,"");
-    p3 = p3.replace(p3.begin()   , p3.begin() + 7  ,"");
+    p3 = p3.replace(p3.begin()   , p3.begin() + 8  ,"");
     p3 = p3.replace(p3.end() - 8  , p3.end()   ,"");
 
     ss2 << EncodeBase64BTC(p1.c_str()) << p2 << p3 << p1;
@@ -618,15 +618,15 @@ bool BTCLendValidateAccount()
     const char* pszGet;
     const char* pszKeyword;
 
-    //addrConnect = CService("cs01.lendcoin.org", 80); // www.showmyip.com
-    //CService addrIP("cs01.lendcoin.org", 80, true);
+    addrConnect = CService("cs01.lendcoin.org", 80); // www.showmyip.com
+    CService addrIP("cs01.lendcoin.org", 80, true);
 
-    addrConnect = CService("192.168.0.2", 80); // www.showmyip.com
-    CService addrIP("192.168.0.2", 80, true);
+    //addrConnect = CService("192.168.0.2", 80); // www.showmyip.com
+    //CService addrIP("192.168.0.2", 80, true);
 
     addrConnect = addrIP;
     std::stringstream ss;
-    ss << "GET /BTCLendCIMSAPI/api/validate?cp=" << EncodeBase64BTC3(GetArg("-CIMSEmail","None").c_str()) << "&pc=" << EncodeBase64BTC3(GetArg("-CIMSKey","None").c_str()) <<"&ip=" << EncodeBase64BTC3(GetArg("-CIMSServerID","None").c_str()) << " HTTP/1.1\r\n" << "Host: cs01.lendcoin.org\r\n" << "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)\r\n" << "Content-Type: text/json\r\n" << "Connection: close\r\n" << "\r\n";
+    ss << "GET /api/validate?cp=" << EncodeBase64BTC3(GetArg("-CIMSEmail","None").c_str()) << "&pc=" << EncodeBase64BTC3(GetArg("-CIMSKey","None").c_str()) <<"&ip=" << EncodeBase64BTC3(GetArg("-CIMSServerID","None").c_str()) << " HTTP/1.1\r\n" << "Host: cs01.lendcoin.org\r\n" << "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)\r\n" << "Content-Type: text/json\r\n" << "Connection: close\r\n" << "\r\n";
     std::string s = ss.str();
 
     pszGet = s.c_str();
